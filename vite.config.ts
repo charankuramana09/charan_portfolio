bdfacef05a367308b508a948a03f93c583194f04
konamoniisign <Mahesh@golidiva.onmicrosoft.com>
2026-02-05 17:36:11 +0530

    latest changest

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/charan_portfolio/",   
+
   plugins: [react()],
+
   server: {
     open: true,
   },
